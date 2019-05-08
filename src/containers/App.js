c8b82b9c85a4fce0e6a10d05804f6959c387cb4a
kristian.angelski <kristian.angelski@gmail.com>
2019-05-08 16:43:01 +0300

    adding cockpit component - holding everything rendered and extraxting the css into cockpit.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 	state = {
@@ -43,45 +43,26 @@ class App extends Component {
 
 	render() {
 		let persons = null;
-		let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					{this.state.persons.map((person, index) => {
-						return (
-							<ErrorBoundary key={person.id}>
-								<Person
-									name={person.name}
-									age={person.age}
-									click={() => this.deletePersonHandler(index)}
-									changed={event => this.nameChangedHandler(event, person.id)}
-								/>
-							</ErrorBoundary>
-						);
-					})}
+					<Persons
+						persons={this.state.persons}
+						clicked={this.deletePersonHandler}
+						changed={this.nameChangedHandler}
+					/>
 				</div>
 			);
-
-			btnClass = classes.Red;
-		}
-
-		const assignedClasses = [];
-
-		if (this.state.persons.length <= 2) {
-			assignedClasses.push(classes.red);
-		}
-		if (this.state.persons.length <= 1) {
-			assignedClasses.push(classes.bold);
 		}
 
 		return (
 			<div className={classes.App}>
-				<h1>Hi, I'm a React App</h1>
-				<p className={assignedClasses.join(' ')}>This is really working!</p>
-				<button className={btnClass} onClick={this.togglePersonsHandler}>
-					Toggle Persons
-				</button>
+				<Cockpit
+					showPersons={this.state.showPersons}
+					persons={this.state.persons}
+					clicked={this.togglePersonsHandler}
+				/>
 				{persons}
 			</div>
 		);
