1624b0983be6c216a424a10da7a13cb6dc46679a
kristian.angelski <kristian.angelski@gmail.com>
2019-05-08 12:13:27 +0300

    adding some conditional stylings

--- src/App.js
+++ src/App.js
@@ -41,8 +41,9 @@ class App extends Component {
 	};
 
 	render() {
-		const btnStyle = {
-			backgroundColor: 'white',
+		const style = {
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
@@ -67,13 +68,25 @@ class App extends Component {
 					})}
 				</div>
 			);
+
+			style.backgroundColor = 'red';
+		}
+
+		const classes = [];
+
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
 		}
 
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
-				<button style={btnStyle} onClick={this.togglePersonsHandler}>
-					show persons
+				<p className={classes.join(' ')}>This is really working!</p>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Toggle Persons
 				</button>
 				{persons}
 			</div>
