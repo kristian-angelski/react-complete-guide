f415ae7e96837c7eba34a48adb1e5a76b53f21cb
kristian.angelski <kristian.angelski@gmail.com>
2019-05-08 15:11:01 +0300

    switching styling to using CSS-MODULES

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
-import './App.css';
-import Radium, { StyleRoot } from 'radium';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -48,11 +47,7 @@ class App extends Component {
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
-			cursor: 'pointer',
-			':hover': {
-				backgroundColor: 'lightgreen',
-				color: 'black'
-			}
+			cursor: 'pointer'
 		};
 
 		let persons = null;
@@ -75,34 +70,28 @@ class App extends Component {
 			);
 
 			style.backgroundColor = 'red';
-			style[':hover'] = {
-				backgroundColor: 'salmon',
-				color: 'black'
-			};
 		}
 
-		const classes = [];
+		const assignedClasses = [];
 
 		if (this.state.persons.length <= 2) {
-			classes.push('red');
+			classes.push(classes.red);
 		}
 		if (this.state.persons.length <= 1) {
-			classes.push('bold');
+			classes.push(classes.bold);
 		}
 
 		return (
-			<StyleRoot>
-				<div className="App">
-					<h1>Hi, I'm a React App</h1>
-					<p className={classes.join(' ')}>This is really working!</p>
-					<button style={style} onClick={this.togglePersonsHandler}>
-						Toggle Persons
-					</button>
-					{persons}
-				</div>
-			</StyleRoot>
+			<div className={classes.App}>
+				<h1>Hi, I'm a React App</h1>
+				<p className={assignedClasses.join(' ')}>This is really working!</p>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Toggle Persons
+				</button>
+				{persons}
+			</div>
 		);
 	}
 }
 
-export default Radium(App);
+export default App;
