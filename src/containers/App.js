a846ae0f58faa2bbfec3123c534947cee925b589
kristian-angelski <kristian.angelski@gmail.com>
2019-05-11 22:09:52 +0300

    slight change to the setState method so that we can be sure that we're working with the latest state, in case it is being changed from some other place in the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
 			{ id: uuid.v4(), name: 'Jane', age: 30 }
 		],
 		showPersons: false,
-		showCockpit: true
+		showCockpit: true,
+		changeCounter: 0
 	};
 
 	static getDerivedStateFromProps(props, state) {
@@ -59,7 +60,9 @@ class App extends Component {
 		const persons = [...this.state.persons];
 		persons[personIndex] = person;
 
-		this.setState({ persons: persons });
+		this.setState((prevState, props) => {
+			return { persons: persons, changeCounter: prevState.changeCounter + 1 };
+		});
 	};
 
 	deletePersonHandler = personIndex => {
